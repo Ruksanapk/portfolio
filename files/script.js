dd8f3d5d4bb4592f114ebc380c9da285e75b897e
Your Name <you@example.com>
2024-07-30 16:44:02 +0530

    updated

--- files/script.js
+++ files/script.js
@@ -1,6 +1,6 @@
 
 var typed = new Typed(".text", {
-    strings: ["Flutter development" , "Web Development"],
+    strings: ["", "Web Development","Flutter development"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
